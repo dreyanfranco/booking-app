a66659423d40ef398e528285d79e689b20446c1a
dreyanfranco <dreyanfranco@gmail.com>
2024-02-08 16:08:42 +0100

    fixed local api-client

--- client/src/service/api-client.ts
+++ client/src/service/api-client.ts
@@ -1,7 +1,6 @@
 import { LoginFormDataProps } from "../pages/Login"
 import { RegisterFormDataProps } from "../pages/Register"
 
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || ""
 
 export const register = async (formData: RegisterFormDataProps) => {
